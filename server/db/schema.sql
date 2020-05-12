379bdc50b7b38c9670cd8e6e3ade68859297179c
Gregor <gregorlennie@hotmail.co.uk>
2020-05-12 09:38:35 +0100

    Defines Postgress database schema

---
+++ server/db/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS budgets;
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    tag_name VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    merchant_name VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE budgets (
+    id SERIAL PRIMARY KEY,
+    budget_name VARCHAR (255),
+    budget_amount DECIMAL(15, 2),
+    warning_limit DECIMAL(15, 2)
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE,
+    tag_id INT REFERENCES tags(id) ON DELETE CASCADE,
+    budget_id INT REFERENCES budgets(id) ON DELETE CASCADE,
+    amount DECIMAL(15, 2),
+    transaction_time TIMESTAMP
+); 
