df1053079d614f015ab39233212cfde9c0b36efc
Gregor <gregorlennie@hotmail.co.uk>
2020-05-12 15:15:38 +0100

    Adds create route for tags

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const pool = require('./db/db');
 app.use(parser.json());
 app.use(cors);
 
-app.get("/tags", async(req, res) => {
+app.get("/tags", async (req, res) => {
     try {
         const allTags = await pool.query("SELECT * FROM tags");
         res.status(200).json(allTags.rows);
@@ -16,9 +16,11 @@ app.get("/tags", async(req, res) => {
     }
 })
 
-app.get("/tags/:id", async(req,res) => {
+app.get("/tags/:id", async (req, res) => {
     try {
-        const { id } = req.params;
+        const {
+            id
+        } = req.params;
         const tag = await pool.query("SELECT * FROM tags WHERE id = $1", [id]);
         res.status(200).json(tag.rows[0]);
     } catch (err) {
@@ -26,6 +28,18 @@ app.get("/tags/:id", async(req,res) => {
     }
 })
 
-app.listen(5000, function() {
+app.post("/tags", async (req, res) => {
+    try {
+        const {
+            tag_name
+        } = req.body;
+        const newTag = await pool.query("INSERT INTO tags (tag_name) VALUES ($1) RETURNING *", [tag_name]);
+        res.status(200).json(newTag.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.listen(5000, function () {
     console.log(`Listening on port ${ this.address().port }`)
 })
