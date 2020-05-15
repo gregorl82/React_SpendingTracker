cd8cef7f9dbf259516c3d4c27eed0cd1d848eaa3
Gregor <gregorlennie@hotmail.co.uk>
2020-05-15 20:18:44 +0100

    Adds server routes for budgets

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
+
+// Middleware libraries
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// Router files
 const categoriesRouter = require('./routers/categoriesRouter');
 const merchantsRouter = require('./routers/merchantsRouter');
+const budgetsRouter = require('./routers/budgetsRouter');
 
+
+// Add middleware
 app.use(bodyParser.json());
 app.use(cors());
 
+
+// Add routers
 app.use("/api/categories", categoriesRouter);
 app.use("/api/merchants", merchantsRouter);
+app.use("/api/budgets", budgetsRouter);
 
 app.listen(5000, () => {
     console.log("Listening on port 5000")
