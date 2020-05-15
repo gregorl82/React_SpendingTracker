27443f15bb3acd76e78f1e741f30d1c7c379a161
Gregor <gregorlennie@hotmail.co.uk>
2020-05-15 11:32:58 +0100

    Writes form input to database

--- client/src/components/TagInput.js
+++ client/src/components/TagInput.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const TagInput = () => {
+
+    const [tag_name, setTagName] = useState("");
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            const body = { tag_name };
+            const response = await fetch("http://localhost:5000/tags", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            })
+            console.log(response);
+            window.location.reload();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
         <Container>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="formTagName">
                     <Form.Label>Enter a new tag</Form.Label>
-                    <Form.Control type="text" placeholder="Tag name" />
+                    <Form.Control
+                        type="text"
+                        placeholder="Tag name"
+                        value={tag_name}
+                        onChange={e => {
+                            setTagName(e.target.value);
+                        }
+                        } />
                 </Form.Group>
-                <Button>Submit</Button>
+                <Button variant="primary" type="submit">Submit</Button>
             </Form>
         </Container>
     )
