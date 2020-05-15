9029288f2ba8b4e59cfb43fd876cc1d7ee5c9378
Gregor <gregorlennie@hotmail.co.uk>
2020-05-15 11:50:00 +0100

    Adds method to delete tags from db

--- client/src/components/TagItem.js
+++ client/src/components/TagItem.js
@@ -3,11 +3,27 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 const TagItem = ({ tag }) => {
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:5000/tags/${id}`, {
+                method: "DELETE"
+            })
+            console.log(response)
+            window.location.reload();
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+
     return (
         <Fragment>
             <Col lg={8}>{tag.tag_name}</Col>
             <Col><Button variant="warning">Edit</Button></Col>
-            <Col><Button variant="danger">Delete</Button></Col>
+            <Col><Button
+                variant="danger"
+                onClick={() => handleDelete(tag.id)}>Delete</Button></Col>
         </Fragment>
     )
 }
