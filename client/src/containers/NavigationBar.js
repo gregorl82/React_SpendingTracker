e5f351c3a66048dca5db7d1009b070968a21fb4f
Gregor <gregorlennie@hotmail.co.uk>
2020-05-17 17:06:15 +0100

    Adds Links to NavigationBar

---
+++ client/src/containers/NavigationBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import NavBar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const NavigationBar = () => {
+
+    const navStyle = {
+        color: "white"
+    }
+
+    return (
+        <NavBar style={{ backgroundColor: 'slategrey' }}>
+            <Link className="p-2" style={navStyle} to="/categories">
+                <Nav>Categories</Nav>
+            </Link>
+            <Link className="p-2" style={navStyle} to="/">
+                <Nav>Merchants</Nav>
+            </Link>
+        </NavBar>
+    )
+}
+
+export default NavigationBar;
