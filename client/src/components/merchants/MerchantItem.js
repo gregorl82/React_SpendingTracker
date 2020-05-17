e0f4803c90af8d244f77e1d8a56b671bdbba2599
Gregor <gregorlennie@hotmail.co.uk>
2020-05-17 18:03:47 +0100

    Connects merchant delete button to db

--- client/src/components/merchants/MerchantItem.js
+++ client/src/components/merchants/MerchantItem.js
@@ -4,11 +4,28 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 const MerchantItem = ({ merchant }) => {
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:5000/api/merchants/${id}`, {
+                method: "DELETE"
+            })
+            console.log(response)
+            window.location.reload();
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
     return (
         <Fragment>
             <Col lg={8}>{merchant.merchant_name}</Col>
             <Col><MerchantEdit merchant={merchant} /></Col>
-            <Col><Button variant="danger">Delete</Button></Col>
+            <Col><Button
+                variant="danger"
+                onClick={() => {
+                    handleDelete(merchant.id)
+                }}>Delete</Button></Col>
         </Fragment>
     )
 }
