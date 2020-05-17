ae467e556e9c5ebd3f3a928d8946af42e7616f8d
Gregor <gregorlennie@hotmail.co.uk>
2020-05-17 17:55:07 +0100

    Connects merchant input to db

--- client/src/components/merchants/MerchantInput.js
+++ client/src/components/merchants/MerchantInput.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const MerchantInput = () => {
+
+    const [merchant_name, setMerchantName] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { merchant_name };
+            const response = await fetch("http://localhost:5000/api/merchants", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            })
+            console.log(response)
+            window.location.reload();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
-        <h2>MerchantInput</h2>
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formMerchantName">
+                    <Form.Label>Enter a new merchant</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Merchant name"
+                        value={merchant_name}
+                        onChange={e => {
+                            setMerchantName(e.target.value);
+                        }
+                        } />
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+        </Container>
     )
 }
 
