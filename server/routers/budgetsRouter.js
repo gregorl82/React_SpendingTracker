1385408b66bb246ce624874f7d7c53f7df27ab60
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-08-17 18:52:55 +0100

    Style code

--- server/routers/budgetsRouter.js
+++ server/routers/budgetsRouter.js
@@ -1,55 +1,67 @@
-const express = require('express');
-const pool = require('../db/db.js');
+const express = require("express");
+const pool = require("../db/db.js");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const allBudgets = await pool.query("SELECT * FROM budgets ORDER BY id ASC");
-        res.status(200).json(allBudgets.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const allBudgets = await pool.query(
+      "SELECT * FROM budgets ORDER BY id ASC"
+    );
+    res.status(200).json(allBudgets.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const budget = await pool.query("SELECT * FROM budgets WHERE id = $1", [id]);
-        res.status(200).json(budget.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { id } = req.params;
+    const budget = await pool.query("SELECT * FROM budgets WHERE id = $1", [
+      id,
+    ]);
+    res.status(200).json(budget.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const { budget_name, budget_amount, warning_limit } = req.body;
-        const newBudget = await pool.query("INSERT INTO budgets (budget_name, budget_amount, warning_limit) VALUES ($1, $2, $3) RETURNING *", [budget_name, budget_amount, warning_limit]);
-        res.status(200).json(newBudget.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { budget_name, budget_amount, warning_limit } = req.body;
+    const newBudget = await pool.query(
+      "INSERT INTO budgets (budget_name, budget_amount, warning_limit) VALUES ($1, $2, $3) RETURNING *",
+      [budget_name, budget_amount, warning_limit]
+    );
+    res.status(200).json(newBudget.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.put("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { budget_name, budget_amount, warning_limit } = req.body;
-        const updateBudget = await pool.query("UPDATE budgets SET (budget_name, budget_amount, warning_limit) = ($1, $2, $3) WHERE id = $4", [budget_name, budget_amount, warning_limit, id])
-        res.status(200).json("Budget was updated!");
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { id } = req.params;
+    const { budget_name, budget_amount, warning_limit } = req.body;
+    const updateBudget = await pool.query(
+      "UPDATE budgets SET (budget_name, budget_amount, warning_limit) = ($1, $2, $3) WHERE id = $4",
+      [budget_name, budget_amount, warning_limit, id]
+    );
+    res.status(200).json("Budget was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteBudget = await pool.query("DELETE FROM budgets WHERE id = $1", [id]);
-        res.status(200).json("Budget was deleted!");
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { id } = req.params;
+    const deleteBudget = await pool.query("DELETE FROM budgets WHERE id = $1", [
+      id,
+    ]);
+    res.status(200).json("Budget was deleted!");
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 module.exports = router;
--- server/routers/categoriesRouter.js
+++ server/routers/categoriesRouter.js
@@ -1,56 +1,70 @@
-const express = require('express');
-const pool = require('../db/db.js');
+const express = require("express");
+const pool = require("../db/db.js");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const allCategories = await pool.query("SELECT * FROM categories ORDER BY id ASC");
-        res.status(200).json(allCategories.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const allCategories = await pool.query(
+      "SELECT * FROM categories ORDER BY id ASC"
+    );
+    res.status(200).json(allCategories.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 router.get("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const category = await pool.query("SELECT * FROM categories WHERE id = $1", [id]);
-        res.status(200).json(category.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+  try {
+    const { id } = req.params;
+    const category = await pool.query(
+      "SELECT * FROM categories WHERE id = $1",
+      [id]
+    );
+    res.status(200).json(category.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 router.post("/", async (req, res) => {
-    try {
-        const { category_name } = req.body;
-        const newCategory = await pool.query("INSERT INTO categories (category_name) VALUES ($1) RETURNING *", [category_name]);
-        res.status(200).json(newCategory.rows[0]);
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+  try {
+    const { category_name } = req.body;
+    const newCategory = await pool.query(
+      "INSERT INTO categories (category_name) VALUES ($1) RETURNING *",
+      [category_name]
+    );
+    res.status(200).json(newCategory.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 router.put("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { category_name } = req.body;
-        const updateCategory = await pool.query("UPDATE categories SET category_name = $1 WHERE id = $2", [category_name, id])
-        res.status(200).json("Category was updated!");
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+  try {
+    const { id } = req.params;
+    const { category_name } = req.body;
+    const updateCategory = await pool.query(
+      "UPDATE categories SET category_name = $1 WHERE id = $2",
+      [category_name, id]
+    );
+    res.status(200).json("Category was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteCategory = await pool.query("DELETE FROM categories WHERE id = $1", [id]);
-        res.status(200).json("Category was deleted!");
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+  try {
+    const { id } = req.params;
+    const deleteCategory = await pool.query(
+      "DELETE FROM categories WHERE id = $1",
+      [id]
+    );
+    res.status(200).json("Category was deleted!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 module.exports = router;
